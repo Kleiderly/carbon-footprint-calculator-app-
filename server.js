0be97bb90a609c0687767e1920ea365d31308cd8
Guilhaxr <guilhaxr123@gmail.com>
2021-09-29 13:55:49 +0100

    changes in route

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 
+const bcrypt = require('bcrypt')
 const path = require('path')
 const express = require('express');
 const mongoose = require('mongoose');
@@ -23,12 +24,15 @@ app.use((req, res, next)=> { res.setHeader("Access-Control-Allow-Origin", "http:
 const Logistic = require('./models/Logistic');
 const Material = require('./models/Material');
 const Fastening = require('./models/Fastening');
+const User = require('./models/User');
 
 
 //Routes
 app.use('/api/logistic', require('./routes/logistic.routes'));
 app.use('/api/fastening', require('./routes/fastening.routes'));
 app.use('/api/material', require('./routes/material.routes'));
+app.use('/api/login'), require('./routes/login.routes');
+app.use('api/register'), require('./routes/register.routes');
 
 
 // Read Frontend from Backend
