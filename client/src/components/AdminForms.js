0860831c51bc2cad6f9a42a14bdf347a66bd82cb
Lorianne Aguilar <lorianne.a.n@gmail.com>
2021-09-29 13:27:33 +0200

    Added comments, and /api to route links

--- client/src/components/AdminForms.js
+++ client/src/components/AdminForms.js
@@ -3,20 +3,20 @@ import axios from "axios";
 import "./CSS/Admin.css";
 
 function AdminForms() {
-
+/* API call results */
     const [material, setMaterial] = useState([]);
     const [logistic, setLogistic] = useState([]);
     const [fastening, setFastening] = useState([]);
-
+/* Chosen values for submission */
     const [modName, setModName] = useState("");
     const [modName2, setModName2] = useState("");
     const [modCo2e, setModCo2e] = useState();
     const [modId, setModId] = useState("");
-    
+/* Chosen object values / Message after submit / Chosen category */
     const [filterArr, setFilterArr] = useState([]);
     const [submit, setSubmit] = useState();
     const [cat, setCat] = useState("");
-
+/* API call links */
     const materialAPI = axios.get(`http://localhost:5000/api/material`);
     const logisticAPI = axios.get(`http://localhost:5000/api/logistic`);
     const fasteningAPI = axios.get(`http://localhost:5000/api/fastening`);
@@ -36,11 +36,12 @@ function AdminForms() {
     const failed = "Submit unsuccessful";
     const clearMessage = filterArr === "";
 
+/* Clear submit message when form is clear */
     useEffect(()=>{
         setSubmit();
     }, [clearMessage]);
 
-    
+/* Fill chosen object values on input change */
     useEffect(()=>{ 
         if(filterArr){
             setModCo2e(filterArr.co2e);
@@ -51,7 +52,7 @@ function AdminForms() {
             }
     }, [filterArr]);
 
-
+/* Clears forms and states */
     const clearForm = ()=>{
         const inputs = document.querySelectorAll("input,select");
         inputs.forEach((item) => (item.value = ""));
@@ -61,10 +62,12 @@ function AdminForms() {
 
 
 /* ROUTES */
+    const postLogistics = `http://localhost:5000/api/${cat}/${modName}/${modName2}/${modCo2e}`;
+    const postOthers = `http://localhost:5000/api/${cat}/${modName}/${modCo2e}`;
+    const modLogistics = `http://localhost:5000/api/${modId}/${modName}/${modName2}/${modCo2e}`;
+    const modOthers = `http://localhost:5000/api/${modId}/${modName}/${modCo2e}`;
 
-    const postLogistics = `http://localhost:5000/${cat}/${modName}/${modName2}/${modCo2e}`;
-    const postOthers = `http://localhost:5000/${cat}/${modName}/${modCo2e}`;
-
+/* POST */
     const handleAdd = (e)=>{
         e.preventDefault();
         axios
@@ -82,11 +85,12 @@ function AdminForms() {
         });
     };
 
+/* PUT */
     const handleModify = (e)=>{
         e.preventDefault();
         axios
         .put(
-          `http://localhost:5000/${cat}/${modId}`
+            cat === "logistic" ? modLogistics : modOthers
         )
         .then((res) => {
             res.send('Item modified');
@@ -99,6 +103,7 @@ function AdminForms() {
         });
     };
 
+/* DELETE */
     const handleDelete = (e)=>{
         e.preventDefault();
         axios
@@ -117,7 +122,7 @@ function AdminForms() {
     };
 
 
-
+/* ADD to X co2e value input HTML */
     const inputCo2e1 = 
         <div className="form-input">
         Co2e per item: <br />
@@ -135,6 +140,7 @@ function AdminForms() {
             <div className="form-section">
                 <h2>Add item to database</h2>
 
+{/* ADD to MATERIALS */}
                 <h4 className="admin-title">Materials</h4>
                 <div className="form-item">
                     <div className="form-input">
@@ -151,7 +157,7 @@ function AdminForms() {
                         {inputCo2e1}
                 </div>
 
-
+{/* ADD to LOGISTICS */}
                 <h4 className="admin-title">Logistics</h4>
                 <div className="form-item">
                     <div className="form-input">
@@ -181,6 +187,7 @@ function AdminForms() {
                     {inputCo2e1}
                 </div>
 
+{/* ADD to FASTENINGS */}
             <h4 className="admin-title">Fastenings</h4>
                 <div className="form-item">
                     <div className="form-input">
@@ -204,9 +211,11 @@ function AdminForms() {
                 <div className="form-submit">&nbsp;{submit}&nbsp;</div>
             </div>
 
+
             <hr className="hr" />
 
-{/* DELETE/MODIFY */}
+
+{/* DELETE/MODIFY FORM */}
             <div className="form-section">
                 <h2>Modify / Delete item from database</h2>
                     
@@ -249,7 +258,7 @@ function AdminForms() {
                     </div>
                 </div>
 
-{/* DELETE/MODIFY LOGISTICS*/}
+{/* DELETE/MODIFY LOGISTICS */}
                 <h4 className="admin-title">Logistics</h4>
                 <div className="form-item">
                     <div className="form-input">
@@ -302,7 +311,7 @@ function AdminForms() {
                     </div>
                 </div>
                 
-{/* DELETE/MODIFY FASTENINGS*/}
+{/* DELETE/MODIFY FASTENINGS */}
                 <h4 className="admin-title">Fastenings</h4>
                 <div className="form-item">
                     <div className="form-input">
@@ -341,6 +350,7 @@ function AdminForms() {
                     </div>
                 </div>
 
+{/* SUBMIT buttons */}
                 <div className="form-input center-align">
                     <button onClick={handleModify}>MODIFY</button>
                     <button onClick={handleDelete}>DELETE</button>
