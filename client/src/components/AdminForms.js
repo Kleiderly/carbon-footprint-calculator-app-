9b81c754a0db8df21a4c98aac29203005b755b67
Lorianne Aguilar <78496780+Grailsidhe@users.noreply.github.com>
2021-10-13 16:39:15 +0200

    Update AdminForms.js

--- client/src/components/AdminForms.js
+++ client/src/components/AdminForms.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from 'react';
 import axios from "axios";
-import "./CSS/Admin.css";
+import "./css/Admin.css";
 
 function AdminForms() {
 /* API call results */
