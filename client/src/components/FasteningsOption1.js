99036daf06ab66dde98dcca46270585c46d91661
AlexHolad <alexej.holad@gmail.com>
2021-10-27 15:45:24 +0200

    select

--- client/src/components/FasteningsOption1.js
+++ client/src/components/FasteningsOption1.js
@@ -1,28 +1,56 @@
-import React, { useState, useEffect, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import axios from "axios";
-import Context from "../contexts/ContextApi";
-import { Link } from "react-router-dom";
-import ProgressBar from "./ProgressBar";
-import "./css/Fastenings.css";
-import Tips from "./Tips";
-import { tipsList } from "./data.js";
-import "./css/vivify.min.css";
-import Select from "react-select";
+import React, { useState, useEffect, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import axios from 'axios';
+import Context from '../contexts/ContextApi';
+import { Link } from 'react-router-dom';
+import ProgressBar from './ProgressBar';
+import './css/Fastenings.css';
+import Tips from './Tips';
+import { tipsList } from './data.js';
+import './css/vivify.min.css';
+import Select from 'react-select';
 
 const FasteningsOption1 = (props) => {
-  const { itemTypeAdress1, setFasteningCO2e1, fasteningCO2e1 } =
-    useContext(Context);
+   const { itemTypeAdress1, setFasteningCO2e1, fasteningCO2e1 } =
+      useContext(Context);
 
-  const [tip, setTip] = useState("");
-  const [fastenings, setFastenings] = useState([]);
+   const [tip, setTip] = useState('');
+   const [fastenings, setFastenings] = useState([]);
 
-  useEffect(() => {
-    axios
-      .get(`http://localhost:5000/api/fastening`)
-      .then((response) => setFastenings(response.data))
-      .catch((error) => {
-        console.log(error);
+   const options = [
+      { value: 1, label: '1' },
+      { value: 2, label: '2' },
+      { value: 3, label: '3' },
+      { value: 4, label: '4' },
+      { value: 5, label: '5' },
+      { value: 6, label: '6' },
+      { value: 7, label: '7' },
+      { value: 8, label: '8' },
+      { value: 9, label: '9' },
+      { value: 10, label: '10' },
+   ];
+
+   useEffect(() => {
+      axios
+         .get(`http://localhost:5000/api/fastening`)
+         .then((response) => setFastenings(response.data))
+         .catch((error) => {
+            console.log(error);
+         });
+   }, []);
+
+   const [listOfQuantities, setlistOfQuantities] = useState([]);
+   const copyOfQuantities = [...listOfQuantities];
+
+   let history = useHistory();
+   const handleClickPreviousSection = () => {
+      history.push('/calculate/materials');
+   };
+
+   useEffect(() => {
+      const temporalArray = [];
+      fastenings.forEach((fastening) => {
+         temporalArray.push({ quantity: 0 });
       });
       setlistOfQuantities([...temporalArray]);
    }, [fastenings]);
@@ -68,7 +96,7 @@ const FasteningsOption1 = (props) => {
                         <p className="fastenings-option-text">
                            {fastening.name}
                         </p>
-                        <input
+                        {/* <input
                            type="number"
                            min="0"
                            step="1"
@@ -76,6 +104,16 @@ const FasteningsOption1 = (props) => {
                            value={listOfQuantities[i].quantity}
                            onChange={(e) => {
                               copyOfQuantities[i].quantity = e.target.value;
+                              setlistOfQuantities([...copyOfQuantities]);
+                           }}
+                        /> */}
+
+                        <Select
+                           options={options}
+                           className="light-pink"
+                           defaultValue={listOfQuantities[i].quantity}
+                           onChange={(e) => {
+                              copyOfQuantities[i].quantity = e.value;
                               setlistOfQuantities([...copyOfQuantities]);
                            }}
                         />
@@ -107,46 +145,7 @@ const FasteningsOption1 = (props) => {
             <Tips category="fastenings" tipObj={tip} />
          </div>
       </div>
-
-      <div>
-        {listOfQuantities.length > 0 &&
-          fastenings.map((fastening, i) => {
-            return (
-              <div key={i}>
-                <p className="fastenings-option-text">{fastening.name}</p>
-                <Select
-                  defaultValue={listOfQuantities[i].quantity}
-                  onChange={(e) => {
-                     copyOfQuantities[i].quantity = e.value;
-                     setlistOfQuantities([...copyOfQuantities]);
-                  }}
-                  options={options}
-                />
-              </div>
-            );
-          })}
-      </div>
-
-      <div className="fastenings-back-next-buttons">
-        <button
-          className="back-button"
-          type="button"
-          onClick={handleClickPreviousSection}
-        >
-          BACK
-        </button>
-        <Link to="/calculate/logistics">
-          <button className="next-button" type="button" onClick={addFastenings}>
-            NEXT
-          </button>
-        </Link>
-      </div>
-
-      <div className="tips">
-        <Tips category="fastenings" tipObj={tip} />
-      </div>
-    </div>
-  );
+   );
 };
 
 export default FasteningsOption1;
