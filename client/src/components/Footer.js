689f85d404800cb736385dbc5f4ea0e507c5516d
AlexHolad <alexej.holad@gmail.com>
2021-10-28 18:20:22 +0200

    mobile footer finished

--- client/src/components/Footer.js
+++ client/src/components/Footer.js
@@ -1,17 +1,58 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import './css/Footer.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./css/Footer.css";
 
 function Footer() {
-   return (
-      <div className="footer-wrapper">
-         <div className="footer-social">FB | INST | TWI</div>
-         <div className="footer-copyrights">© 2021 KLEIDERLY</div>
-         <div className="footer-admin">
-            <Link to="/adminpage/login">ADMIN</Link>
-         </div>
+  return (
+    <div className="footer-wrapper">
+      <div className="footer-copyrights">
+        <p>
+          Kleiderly UG (haftungsbeschränkt)
+          <br />
+          c/o Factory Mitte HQ. <br />
+          Rheinsberger Str. 76/77 . 10115 Berlin
+          <br />
+          Amtsgericht Berlin (Charlottenburg) HRB 214291
+          <br />
+        </p>
       </div>
-   );
+      <div>
+        <img
+          className="footer-logo"
+          src="../img/kleiderly_logo.png"
+          alt="Logo"
+        />
+      </div>
+      <div className="footer-logo-impressum">
+      <div className="footer-impressum"><a href='https://kleiderly.com/pages/impressum' target='_blank' rel='noreferrer'><p>Impressum</p></a></div>
+      <div className="footer-social">
+        <a href='https://www.instagram.com/kleiderlyberlin/' target='_blank' rel='noreferrer'><img
+          className="footer-social-icon"
+          src="../img/social-icons/instagram.png"
+          alt="Logo"
+        /></a>
+        <a href='https://www.facebook.com/kleiderly/' target='_blank' rel='noreferrer'><img
+          className="footer-social-icon"
+          src="../img/social-icons/facebook.png"
+          alt="Logo"
+        /></a>
+        <a href='https://twitter.com/kleiderlyberlin' target='_blank' rel='noreferrer'><img
+          className="footer-social-icon"
+          src="../img/social-icons/twitter.png"
+          alt="Logo"
+        /></a>
+        <a href='https://www.linkedin.com/company/kleiderlyberlin/' target='_blank' rel='noreferrer'><img
+          className="footer-social-icon"
+          src="../img/social-icons/linkedin.png"
+          alt="Logo"
+        /></a>
+      </div>
+      {/* <div className="footer-admin">
+        <Link to="/adminpage/login">ADMIN</Link>
+      </div> */}
+    </div>
+    </div>
+  );
 }
 
 export default Footer;
