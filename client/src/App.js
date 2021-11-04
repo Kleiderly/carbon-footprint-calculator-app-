9e962f17cce76f587929fa1ee092363f4170d9b9
AlexHolad <alexej.holad@gmail.com>
2021-11-04 08:34:50 +0100

    build

--- client/src/App.js
+++ client/src/App.js
@@ -52,11 +52,11 @@ function App() {
     const [loader, setLoader] = useState("../img/KLEIDERLY_favicon.png");
 
     // LOADER USEEFFECT
-    // useEffect(() => {
-    //   setTimeout(() => {
-    //     setLoader();
-    //   }, 3000);
-    // }, []);
+    useEffect(() => {
+      setTimeout(() => {
+        setLoader();
+      }, 3000);
+    }, []);
 
   return (
     <div className="App">
@@ -93,7 +93,6 @@ function App() {
         >{loader ? (
           <div className='divLoader'>
             <img src={loader} alt={loader} className="loader"/>
-            {/* <p class="header-title">Fashion Footprint</p> */}
           </div>
         ) : (
           <div>
